d8b37c174396881f11037df4290ed290b3fbd030
Ismael <ismael.ka@protonmail.com>
2023-11-16 13:31:25 +0300

    initialize project file

---
+++ src/main/java/io/github/ismaele77/LiveMinds/LiveMindsApplication.java
@@ -0,0 +1,13 @@
+package io.github.ismaele77.LiveMinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveMindsApplication.class, args);
+	}
+
+}
